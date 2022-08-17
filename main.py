31c06be5b4b8ad8807f0c3fb2502bc2d59a63a1c
isabel <igarc770@gmail.com>
2022-08-17 01:04:18 -0700

    agregar nueva funcion para obtener token

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+import json
+import requests
 import conf
-print(conf.clave)
-print(conf.usuario)
+
+
+def obtener_token(usuario, clave):
+    url = "https://10.10.20.14/api/aaaLogin.json"
+    body = {
+        "aaaUser": {
+            "attributes": {
+                "name": usuario,
+                "pwd": clave
+            }
+        }
+    }
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    response = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False).json()
+
+    token = response['imdata'][0]['aaaLogin']['attributes']['token']
+    return token
+
+token = obtener_token(conf.usuario, conf.clave)
+
+print(token)
