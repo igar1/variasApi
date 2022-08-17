282c234a78ea008e7bd0e2b2fc8c925fcab9e861
isabel <igarc770@gmail.com>
2022-08-17 04:44:13 -0700

    agregar 2 recursos nuevos

--- main.py
+++ main.py
@@ -17,11 +17,41 @@ def obtener_token(usuario, clave):
         "Content-Type": "application/json"
     }
     requests.packages.urllib3.disable_warnings()
-    response = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False).json()
 
+    response = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False).json()
+    print(response)
     token = response['imdata'][0]['aaaLogin']['attributes']['token']
+
     return token
 
 token = obtener_token(conf.usuario, conf.clave)
 
-print(token)
+def top_level_system():
+    sandbox = "https://10.10.20.14/api/aaaLogin.json"
+    url = sandbox + "/api/class/topSystem.json"
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    galletas = {
+        "APIC-Cookie": token
+    }
+    respuesta = requests.get(url, headers=cabecera, cookies=galletas, verify=False)
+    print(respuesta.json())
+
+
+top_level_system()
+
+#GET http://apic-ip-address/api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json
+
+def about_node():
+    url = "https://10.10.20.14/api/aaaLogin.json"
+
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    print(requests)
+    respuesta = requests.get(url+"api/mo/topology/pod-1/node-1/sys/ch/bslot/board/sensor-3.json", headers=cabecera, verify=False)
+    print(respuesta.json())
+
+about_node()
